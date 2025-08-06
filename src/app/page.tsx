032429a0d596871ea148c8c5e3b08ff71b96ed0b
Estanlei Sousa <estanlei2020@gmail.com>
2025-08-05 22:32:18 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <button className="my-1 bg-red-500 px-5">bootcamp</button>
+      <button>bootcamp</button>
     </div>
   );
 }
